4a42a5b704a3f01ccab3b9a0d2877d7d6e4b131e
Timuur <timur2st@gmail.com>
2024-04-17 20:08:23 +0500

    Добавьте файлы проекта.

---
+++ oop_4/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace oop_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(169, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(619, 426);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Множ.Выбор",
+            "Нажата ли Ctrl"});
+            this.checkedListBox1.Location = new System.Drawing.Point(12, 150);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 94);
+            this.checkedListBox1.TabIndex = 4;
+            this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+    }
+}
+
---
+++ oop_4/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Configuration;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace oop_4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        class CCircle
+        {
+            private int x, y;
+            private const int radius = 40;
+            public bool selected = false;
+            private bool deleted = false;
+            public CCircle() { x = 0; y = 0; }
+            public CCircle(int x, int y) { this.x = x; this.y = y; }
+            public CCircle(CCircle circle) { x = circle.x; y = circle.y; }
+            ~CCircle() { }
+            public void DrawMe(PaintEventArgs e)
+            {
+                Pen pen = new Pen(Color.Brown, 3);
+                if (selected && !deleted)
+                {
+                    e.Graphics.DrawEllipse(pen, x - radius, y - radius, radius * 2, radius * 2);
+                    e.Graphics.FillEllipse(Brushes.Blue, x - radius, y - radius, radius * 2, radius * 2);
+                }
+                else if (deleted)
+                {
+                    pen.Color = SystemColors.Control;
+                    e.Graphics.DrawEllipse(pen, x - radius, y - radius, radius * 2, radius * 2);
+                    e.Graphics.FillEllipse(SystemBrushes.Control, x - radius, y - radius, radius * 2, radius * 2);
+                }
+                else
+                {
+                    e.Graphics.DrawEllipse(pen, x - radius, y - radius, radius * 2, radius * 2);
+                    e.Graphics.FillEllipse(Brushes.Black, x - radius, y - radius, radius * 2, radius * 2);
+                }
+            }
+            public bool SelectionCheck(MouseEventArgs e, bool CrtlPressed, bool MultiplySelections)
+            {
+                if ((e.X > x - radius && e.X < x + radius) && (e.Y > y - radius && e.Y < y + radius))
+                {
+                    if (MultiplySelections) { selected = true; }
+                    return true;
+                }
+                else
+                {
+                    if (!CrtlPressed) { selected = false; }
+                    return false;
+                }
+            }
+            public bool IsSelected()
+            {
+                if (selected)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            public void DeleteMeIfSelected()
+            {
+                if (selected)
+                {
+                    deleted = true;
+                }
+            }
+            public int getRLength(MouseEventArgs e)
+            {
+                return (int)Math.Sqrt(Math.Pow((e.X - x), 2) + Math.Pow((e.Y - y), 2));
+            }
+        }
+        class container
+        {
+            CCircle[] array;
+            private int size, capacity;
+            public container() { size = 0; capacity = 1; array = new CCircle[capacity]; }
+            public container(int capacity) { size = 0; this.capacity = capacity; array = new CCircle[capacity]; }
+            public container(container c) { size = c.size; capacity = c.capacity; array = new container(c).array; }
+            ~container() { }
+            public void Push_back(CCircle element)
+            {
+                if (size >= capacity)
+                {
+                    capacity *= 2;
+                    CCircle[] newArr = new CCircle[capacity];
+                    for (int i = 0; i < size; i++)
+                    {
+                        newArr[i] = array[i];
+                    }
+                    array = newArr;
+                }
+                array[size++] = element;
+            }
+            public void DrawAll(PaintEventArgs e)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    array[i].DrawMe(e);
+                }
+            }
+            public bool SelectionCheckAll(MouseEventArgs e, bool CtrlPressed, bool MultiplySelections)
+            {
+                bool anyIsSelected = false;
+                if (MultiplySelections)
+                {
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (array[i].SelectionCheck(e, CtrlPressed, MultiplySelections) == true) { anyIsSelected = true; }
+                    }
+                }
+                else
+                {
+                    int minRLength = 10000;
+                    CCircle nearestObj = null;
+                    for (int i = 0; i < size; i++)
+                    {
+                        if (array[i].SelectionCheck(e, CtrlPressed, MultiplySelections) == true)
+                        {
+                            anyIsSelected = true;
+                            if (array[i].getRLength(e) < minRLength)
+                            {
+                                nearestObj = array[i];
+                            }
+
+                        }
+                    }
+                    if (nearestObj != null)
+                    {
+                        if (!CtrlPressed) { MakeAllObjsUnselected(); }
+                        nearestObj.selected = true;
+                    }
+
+                }
+                return anyIsSelected;
+            }
+            public void DeleteSelectedFromContainer()
+            {
+                int const_size = size;
+                for (int i = 0; i < const_size; i++)
+                {
+                    if (array[i] != null)
+                    {
+                        if (array[i].IsSelected())
+                        {
+                            DeleteObjFromContainer(array[i]);
+                            i--;
+                        }
+                    }
+                }
+            }
+            public void DeleteObjFromContainer(CCircle CircleToDelete)
+            {
+                CCircle[] newArr = new CCircle[capacity];
+                bool elemFind = false;
+                size--;
+                for (int i = 0; i < size; i++)
+                {
+                    if (array[i] == CircleToDelete)
+                    {
+                        elemFind = true;
+                    }
+                    if (elemFind) { newArr[i] = array[i + 1]; }
+                    else { newArr[i] = array[i]; }
+                }
+                if (array[size] == CircleToDelete) { elemFind = true; } //Для последнего элемента
+                array = newArr;
+                if (!elemFind) { throw new Exception("ObjNotFoundInContainer"); }
+            }
+            public void MakeSelectedObjsDeleted()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    array[i].DeleteMeIfSelected();
+                }
+            }
+            public void MakeLastObjSelected()
+            {
+                if (size > 0)
+                {
+                    array[size - 1].selected = true;
+                }
+            }
+            public void MakeAllObjsUnselected()
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    array[i].selected = false;
+                }
+            }
+        }
+
+        container c = new container();
+        bool CtrlPressed = false;
+        bool MultiplySelections = false;
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (!c.SelectionCheckAll(e, CtrlPressed, MultiplySelections))
+            {
+                CCircle createdCircle = new CCircle(e.X, e.Y);
+                c.Push_back(createdCircle);
+                createdCircle.selected = true;
+            }
+            this.Refresh();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            c.DrawAll(e);
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.ControlKey)
+            {
+                label2.Text = "CtrlPressed = false";
+                CtrlPressed = false;
+            }
+            if (e.KeyCode == Keys.Delete)
+            {
+                c.MakeSelectedObjsDeleted();
+                c.DeleteSelectedFromContainer();
+                c.MakeLastObjSelected();
+                this.Refresh();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //if (e.KeyChar == Keys.ControlKey || checkBox2.Checked)
+            //{
+            MessageBox.Show(e.KeyChar.ToString());
+            label2.Text += e.KeyChar.ToString();
+            //"CtrlPressed = true";
+            //CtrlPressed = true;
+            //}
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (checkedListBox1.CheckedIndices.Contains(1))
+            {
+                label1.Text = "MultiplySelections = true";
+                MultiplySelections = true;
+            }
+            else
+            {
+                label1.Text = "MultiplySelections = false";
+                MultiplySelections = false;
+            }
+        }
+    }
+}
